94fbf8aa327536d35b3aa4bfe082af00d2490ca6
Chaitanya <chaitanya@debian>
2021-06-26 14:08:37 +0530

    Added push, pop, join, append operations.

--- ds.h
+++ ds.h
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stddef.h>
 
+#include <stdio.h>
+
+typedef char* dstr;
 
 struct ds_string {
   size_t len;
@@ -27,6 +30,7 @@ __fill_str(struct ds_string *new_str, const char *contents,
   if (pos >= strlen(contents)) return;
 
   strncat(new_str->contents, contents+pos, len);
+  new_str->len = len;
 }
 
 static char *
@@ -38,16 +42,38 @@ __new_base(size_t size, size_t len,
   if (!new_str) return NULL;
 
   if (contents) __fill_str(new_str, contents, len, pos); 
-
-  return (char *)new_str + offsetof(struct ds_string, contents);
+  new_str->size = size;
+  return new_str->contents;
 }
+
+static struct ds_string *
+__ds_relocate(struct ds_string *container, size_t new_size)
+{
+  struct ds_string *old = container;
+  if (new_size == 0) new_size = 2*old->size;
+
+  container = malloc(sizeof(struct ds_string)
+		     + (sizeof(char)*new_size));
+  if (container == NULL) return NULL;
+
+  container->size = new_size;
+  container->len = old->len;
+  strncat(container->contents, old->contents, container->len);
+  
+  free(old);
+
+  return container;
+}
+
+#define get_container(str) (struct ds_string *) \
+    (str - offsetof(struct ds_string, contents))
 
 
 char *
 __new_var(struct ds_new_args args)
 {
   /* filling in default values if not provided */
-  size_t size = args.size ? args.size : 16;
+  size_t size = args.size > 16 ? args.size : 16;
   size = size > args.len ? size : args.len;
   size_t len = args.len ? args.len : 0;
   size_t pos = args.pos ? args.pos : 0;
@@ -63,11 +89,67 @@ __new_var(struct ds_new_args args)
 #define ds_new(...) __new_var((struct ds_new_args){__VA_ARGS__})
 
 void
-ds_free(char *str)
+ds_free(dstr str)
 {
-  struct ds_string *container;
-  container = (struct ds_string *)
-    (str - offsetof(struct ds_string, contents));
+  struct ds_string *container = get_container(str);
   free(container);
   return;
+}
+
+/* push, append, and join take ownership of the pointer */
+char *
+ds_push(dstr str, char c)
+{
+  struct ds_string *container = get_container(str);
+
+  if (container->size <= container->len+2) {
+    container = __ds_relocate(container, 0);
+  }
+
+  container->contents[container->len] = c;
+  container->contents[container->len+1] = '\0';
+  ++container->len;
+  
+  return container->contents;
+}
+
+void
+ds_pop(dstr str)
+{
+  struct ds_string *container = get_container(str);
+
+  if (container->len == 0) return;
+  container->contents[container->len-1] = '\0';
+  --container->len;
+}
+
+char *
+ds_append(dstr dest, const dstr cat)
+{
+  struct ds_string *dst_cont = get_container(dest);
+  struct ds_string *cat_cont = get_container(cat);
+
+  if (dst_cont->size <= dst_cont->len + cat_cont->len + 1) {
+    size_t fac = (dst_cont->len + cat_cont->len + 1)/16 + 1;
+    fac = fac < 2 ? 2 : fac;
+
+    __ds_relocate(dst_cont, fac*dst_cont->size);
+  }
+
+  strncat(dst_cont->contents + dst_cont->len, cat_cont->contents,
+	  cat_cont->len);
+  
+  return dst_cont->contents;
+}
+
+char *
+ds_join(dstr dest, const dstr *str_arr, size_t len, dstr delim)
+{
+  for (size_t i = 0; i < len-1; ++i) {
+    dest = ds_append(dest, str_arr[i]);
+    dest = ds_append(dest, delim);
+  }
+  dest = ds_append(dest, str_arr[len-1]);
+  
+  return dest;
 }
--- test.c
+++ test.c
@@ -5,11 +5,16 @@
 
 #include "ds.h"
 
-enum Tests {create=10, destroy=20, all=0};
+enum Tests {create=10, destroy=20, push=30,
+            append=40, pop=50, join=60, all=0};
 enum Result {SUCCESS=0, FAIL=1};
 
-int test_new_str();
-int test_free_str();
+int test_new_str(void);
+int test_free_str(void);
+int test_push(void);
+int test_pop(void);
+int test_append(void);
+int test_join(void);
 
 enum Tests getTest(char *name)
 {
@@ -17,18 +22,34 @@ enum Tests getTest(char *name)
     return create;
   } else if (!strncmp(name, "dst", 3)) {
     return destroy;
-  } else {
-    return all;
+  } else if (!strncmp(name, "pus", 3)) {
+    return push;
+  } else if (!strncmp(name, "pop", 3)) {
+    return pop;
+  } else if (!strncmp(name, "app", 3)) {
+    return append;
+  } else if (!strncmp(name, "jon", 3)) {
+    return join;
   }
+  return all;
 }
 
-int selector(enum Tests sel)
+int
+selector(enum Tests sel)
 {
   switch(sel) {
   case create:
     return test_new_str();
   case destroy:
     return test_free_str();
+  case append:
+    return test_append();
+  case join:
+    return test_join();
+  case push:
+    return test_push();
+  case pop:
+    return test_pop();
   case all:
     return test_new_str() + test_free_str();
   default:
@@ -36,15 +57,21 @@ int selector(enum Tests sel)
   }
 }
 
-char * getOption(int argc, char **argv)
+char *
+getOption(int argc, char **argv)
 {
-  if (argc < 1) return "all";
+  if (argc <= 1) return "all";
   if (!strncmp(argv[1], "-c", 2)) return "crt";
   if (!strncmp(argv[1], "-d", 2)) return "dst";
+  if (!strncmp(argv[1], "-pu", 3)) return "pus";
+  if (!strncmp(argv[1], "-po", 3)) return "pop";
+  if (!strncmp(argv[1], "-ap", 3)) return "app";
+  if (!strncmp(argv[1], "-j", 2)) return "jon";
   return "non";
 }
 
-int main (int argc, char **argv)
+int
+main (int argc, char **argv)
 {
   char *opt = getOption(argc, argv);
   if (!strncmp(opt, "non", 3)) return 1;
@@ -59,14 +86,11 @@ int main (int argc, char **argv)
 }
 
 /* Test wether a new dynamic string has been created with the contents */
-int test_new_str() {
-  char *test_contents_ctor = ds_new(.contents="hi all");
-  char *test_substr_ctor = ds_new(.contents="hi all this", .len=6);
-  char *test_substr_ctor1 = ds_new(.contents="hi all this", .len=3, .pos=3);
-
-  printf("%s\n", test_contents_ctor);
-  printf("%s\n", test_substr_ctor);
-  printf("%s\n", test_substr_ctor1);
+int
+test_new_str(void) {
+  dstr test_contents_ctor = ds_new(.contents="hi all");
+  dstr test_substr_ctor = ds_new(.contents="hi all this", .len=6);
+  dstr test_substr_ctor1 = ds_new(.contents="hi all this", .len=3, .pos=3);
 
   if (strncmp(test_contents_ctor, "hi all", 6)) return FAIL;
   if (strncmp(test_substr_ctor, "hi all", 6)) return FAIL;
@@ -78,9 +102,67 @@ int test_new_str() {
 /* Test wether the dynamic string pointer has been freed or not. */
 /* No way to test wether a pointer is freed or not but in gdb it 
 shows that correct pointer was freed. */
-int test_free_str()
+int
+test_free_str(void)
 {
-  char *test_free = ds_new(.contents="hi all");
+  dstr test_free = ds_new(.contents="hi all");
   ds_free(test_free);
+  return SUCCESS;
+}
+
+int
+test_push(void)
+{
+  dstr target = "this is a test..........";
+  dstr str = ds_new(.contents="this is a test");
+
+  for (int i = 0; i < 10; ++i) str = ds_push(str, '.');
+
+  if (strlen(str) != strlen(target)) return FAIL;
+  if (strncmp(str, target, strlen(str))) return FAIL;
+
+  return SUCCESS;
+}
+
+int
+test_pop(void)
+{
+  dstr str = ds_new(.contents="test");
+
+  for (int i = 0; i < 3; ++i) ds_pop(str);
+
+  if (strlen(str) != 1) return FAIL;
+  if (strncmp(str, "t", strlen(str))) return FAIL;
+
+  return SUCCESS;
+}
+
+int
+test_join(void)
+{
+  const dstr str_arr[] = {ds_new(.contents="this"), ds_new(.contents="is"),
+                     ds_new(.contents="a"), ds_new(.contents="test")};
+
+  dstr res = ds_join(ds_new(), str_arr, 4, ds_new(.contents=" "));
+  dstr target = "this is a test";
+
+  if (strlen(res) != strlen(target)) return FAIL;
+  if (strncmp(res, target, strlen(target)+1)) return FAIL;
+
+  return SUCCESS;
+}
+
+int
+test_append(void)
+{
+  dstr str1 = ds_new(.contents="this ");
+  dstr str2 = ds_new(.contents="is a test with some long string");
+  dstr target = "this is a test with some long string";
+  str1 = ds_append(str1, str2);
+
+  if (strlen(str1) != strlen(target))
+    return FAIL;
+  if (strncmp(str1, target, strlen(target))) return FAIL;
+
   return SUCCESS;
 }
